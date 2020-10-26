5107c7cc1753393b0f84e22cf532701a4e758282
nubnist <44030910+nubnist@users.noreply.github.com>
2020-10-26 21:35:56 +0300

    Добавьте файлы проекта.

---
+++ Core/Explorer.Shared.ViewModels/MainViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Explorer.Shared.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        #region PublicProperties
+
+        public string MainDiskName { get; set; }
+
+        #endregion
+
+        #region Events
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
+
+        #region Constructor
+
+        public MainViewModel()
+        {
+            MainDiskName = Environment.SystemDirectory;
+        }
+
+        #endregion
+
+        #region ProtectedMethods
+
+        private void OnPropertyChanged([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        #endregion
+    }
+}
